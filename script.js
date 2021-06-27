c33f09b7ea0fd62eca2c618c000f2e5b92ca2c49
Steven Redman <StevenRedman122@gmail.com>
2021-06-27 23:40:26 +0100

    Added Monochrome theme

--- script.js
+++ script.js
@@ -6,17 +6,43 @@ const gridSizeSlider = document.querySelector(`#grid-size-slider`);
 const gridSizeSliderLabel = document.querySelector(`#grid-size-slider-label`);
 const clearButton = document.querySelector(`.clear-button`);
 const createGridButton = document.querySelector(`.create-grid-button`);
+const standardRadioButton=document.querySelector(`#standard-theme`);
+const monochromeRadioButton=document.querySelector(`#monochrome-theme`);
+
+monochromeRadioButton.addEventListener(`click`,()=>{
+    let root = document.documentElement;
+    root.style.setProperty(`--grid-background-color`,`#4ea96b`);
+    root.style.setProperty(`--empty-cell-color`,`#4ea96b`);
+    root.style.setProperty(`--full-cell-color`,`black`);
+    root.style.setProperty(`--background-color`,`#4ea96b`);
+    root.style.setProperty(`--settings-background-color`,`#4ea96b`);
+    root.style.setProperty(`--text-color`,`black`);
+    root.style.setProperty(`--border-color`,`black`);
+})
+
+
+standardRadioButton.addEventListener(`click`,()=>{
+    let root = document.documentElement;
+    root.style.setProperty(`--grid-background-color`,`black`);
+    root.style.setProperty(`--empty-cell-color`,`black`);
+    root.style.setProperty(`--full-cell-color`,`white`);
+    root.style.setProperty(`--background-color`,`black`);
+    root.style.setProperty(`--settings-background-color`,`black`);
+    root.style.setProperty(`--text-color`,`white`);
+    root.style.setProperty(`--border-color`,`white`);
+})
+
 
 clearButton.addEventListener(`click`, clearGrid);
 createGridButton.addEventListener(`click`, () => { clearGrid(); createGrid() });
 gridSizeSlider.oninput = () => { gridSizeSliderLabel.textContent = `Cells per grid axis: ${gridSizeSlider.value}` };
 
 function clearGrid() {
-    const gridCellsToClean = document.querySelectorAll(`.grid-cell`);
+    const gridCellsToClean = document.querySelectorAll(`.full-grid-cell`);
 
     gridCellsToClean.forEach(cell => {
-        cell.style.setProperty("background-color", "black");
-
+        cell.classList.remove(`full-grid-cell`);
+        cell.classList.add(`empty-grid-cell`);
     });
 
 }
@@ -37,8 +63,12 @@ function createGrid() {
         gridContainer.setAttribute(`style`, `width:${gridSizeInPixels}px;height:${gridSizeInPixels}px;display:grid;grid-template-columns:repeat(${gridSizePerAxis},${cellSize}px);`);
         let gridCell = document.createElement(`div`);
         gridCell.classList.add(`grid-cell`);
-        gridCell.addEventListener(`mouseenter`, () => { gridCell.style.setProperty("background-color", "white"); });
+        gridCell.classList.add(`empty-grid-cell`);
+        gridCell.addEventListener(`mouseenter`, () => { gridCell.classList.remove(`empty-grid-cell`);gridCell.classList.add(`full-grid-cell`); });
         gridContainer.appendChild(gridCell);
 
     }
 }
+
+
+
